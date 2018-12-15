c269be9a6ab5546aec2ad4214f888e16ecb62108
Túlio <tuliofranca@visionnaire.com.br>
2018-12-15 12:28:19 -0200

    Add marker functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
+/// Adding Google Maps to Flutter
+/// https://medium.com/flutter-io/google-maps-and-flutter-cfb330f9a245
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   GoogleMapController mapController;
+  MapType _currentMapType = MapType.normal;
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
-
-  MapType _currentMapType = MapType.normal;
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -28,6 +30,20 @@ class MyApp extends StatelessWidget {
     }
   }
 
+  void _onAddMarkerButtonPressed() {
+    mapController.addMarker(
+      MarkerOptions(
+        position: LatLng(
+          mapController.cameraPosition.target.latitude,
+          mapController.cameraPosition.target.longitude,
+        ),
+        infoWindowText: InfoWindowText('Random Place', '5 Star Rating'),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor
+            .hueViolet), //BitmapDescriptor.fromAsset('assets/pitch.png'),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -45,20 +61,37 @@ class MyApp extends StatelessWidget {
                   target: _center,
                   zoom: 11.0,
                 ),
+                trackCameraPosition: true,
               ),
             ),
             Padding(
               padding: const EdgeInsets.all(16.0),
               child: Align(
                 alignment: Alignment.topRight,
-                child: FloatingActionButton(
-                  onPressed: _onMapTypeButtonPressed,
-                  materialTapTargetSize: MaterialTapTargetSize.padded,
-                  backgroundColor: Colors.green,
-                  child: const Icon(
-                    Icons.map,
-                    size: 36.0,
-                  ),
+                child: Column(
+                  children: <Widget>[
+                    FloatingActionButton(
+                      onPressed: _onMapTypeButtonPressed,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      backgroundColor: Colors.green,
+                      child: const Icon(
+                        Icons.map,
+                        size: 36.0,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    FloatingActionButton(
+                      onPressed: _onAddMarkerButtonPressed,
+                      materialTapTargetSize: MaterialTapTargetSize.padded,
+                      backgroundColor: Colors.green,
+                      child: const Icon(
+                        Icons.add_location,
+                        size: 36.0,
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ),
